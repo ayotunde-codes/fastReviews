b2b59d40752d076782def95937b10d96006bc108
Ayotunde Obasa <ayobam222@gmail.com>
2022-07-06 10:41:35 +0100

    new stage

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">FastReview</a>
+          Welcome to <a href="https://nextjs.org">FastReviews</a>
         </h1>
 
         <p className={styles.description}>
